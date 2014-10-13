cb561a178f5c557fe66b60f86889adcb12a7aab5
SnowdogU77 <underdweller@gmail.com>
2014-10-13 13:26:02 -0500

    Initial commit

---
+++ alphaciv/constants.py
@@ -0,0 +1,14 @@
+# By: Jarren, Stetson, and Luke
+# constants.py
+
+BLUE = 'blue'
+RED = 'red'
+
+PLAINS = 'plains'
+OCEANS = 'oceans'
+FORESTS = 'forests'
+MOUNTAINS = 'mountains'
+HILLS = 'hills'
+CITY = 'city'
+FOOD = 'food'
+PRODUCTION = 'production'
---
+++ alphaciv/test_alphaciv.py
@@ -0,0 +1,4 @@
+#!python2
+# By: Jarren, Stetson, and Luke
+# test_alphaciv.py
+
